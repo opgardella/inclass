fd35213e29e6cdb6c965987a3a678e0b25267918
Olivia Gardella <gardella@umich.edu>
2017-10-02 15:20:44 -0400

    adding beautiful soup in class exercises

---
+++ beautifulsoup.py
@@ -0,0 +1,8 @@
+#beautiful soup in class
+
+from bs4 import BeautifulSoup
+
+with open("index.html") as fp:
+    soup = BeautifulSoup(fp)
+
+soup = BeautifulSoup("<html>data</html>")
--- regularexp.py
+++ regularexp.py
@@ -1,3 +1,13 @@
 import re
 
-with open("mbox-sort.txt")
+values = []
+with open("mbox-short.txt",'r') as data:
+    for line in data:
+        if re.findall('X-DSPAM-Confidence:', line):
+            #print (line)
+            values.append(re.findall((":(.\S+)"), line))
+
+print (values)
+print ("Number of values:" + str(len(values)))
+print ("Maximum:" + str(max(values)[0]))
+print ("Minimum:" + str(min(values)[0]))
