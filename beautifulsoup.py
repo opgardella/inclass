7cf12c6f2bbbbe2905b9eae94b23b78959be89bc
Olivia Gardella <gardella@umich.edu>
2017-10-09 14:44:34 -0400

    adding class files for 10/9

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -1,8 +1,6 @@
 #beautiful soup in class
 
-from bs4 import BeautifulSoup
-
-with open("index.html") as fp:
-    soup = BeautifulSoup(fp)
-
-soup = BeautifulSoup("<html>data</html>")
+#from bs4 import BeautifulSoup
+#with open("index.html") as fp:
+#    soup = BeautifulSoup(fp)
+#soup = BeautifulSoup("<html>data</html>")
