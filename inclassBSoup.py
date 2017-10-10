96450d4233cb3e52f62fbd895b7d29a98a05c4be
Olivia Gardella <gardella@umich.edu>
2017-10-09 20:14:31 -0400

    in class files complete

--- inclassBSoup.py
+++ inclassBSoup.py
@@ -3,46 +3,62 @@ from bs4 import BeautifulSoup
 #YOUR CODE 1
 # open the "samplehtml.html" file and read it into a string
 # No ouptput
+f = open('samplehtml.html', 'r')
+text = f.read() #must read it! takes the file object and turns it into a string you can do reg expression searches on
+f.close()
 
 print("\n")
 
 #YOUR CODE 2
 # create a BeautifulSoup object from the string
 # No output
+soup = BeautifulSoup(text, 'html.parser')
+#print (soup.find_all('img'))
+imgs = soup.find_all('img')
+#print(imgs) to see how long it is
+xkcd_img = imgs[0] #since we know its only one image
+#with find_all you are returned a list (usually of dictionaries)
 
 print("\n")
 
 #YOUR CODE 3
 # Write/plan code to print the URL to the XKCD comic, using BSoup and this file.
+print(xkcd_img['src'])
 
 print("\n")
 
 #YOUR CODE 4
 # Write code to grab all the links (URLs) in that html page, but nothing else.
-
+for lnk in soup.find_all('a'):
+    print(lnk['href'])
 
 print("\n")
 
 #YOUR CODE 5
 # Write/plan code to grab the TEXT of all the links in that html page, but nothing else.
-
+for lnk in soup.find_all('a'):
+    print(lnk.text)
 
 print("\n")
 #YOUR CODE 6
-# Write/plan code to grab the text of each of the items in the ordered list. (Not the 1/2/2, just the text. 
-
+# Write/plan code to grab the text of each of the items in the ordered list. (Not the 1/2/2, just the text)
+list_objs = soup.find_all('li')
+for li in list_objs:
+    print(li.text)
 
 print("\n")
 #YOUR CODE 7
 # - Write/plan code to grab the alt text associated with the image of the XKCD comic.
 # see way above for accessing xkcd_img...
-# OUTPUT - 
+# OUTPUT -
+print(xkcd_img['aria-text'])
 
 print("\n")
 #YOUR CODE 7
-# Write  code to assign the BeautifulSoup object holding the div that contains the image to a variable `image_div_obj` 
+# Write  code to assign the BeautifulSoup object holding the div that contains the image to a variable `image_div_obj`
 #Ouput: print image_div_obj
-
+image_div_obj = soup.find('div, {'id':'below-section'})
+print(image_div_obj)
 
 
 print("\n")
@@ -54,6 +70,7 @@ print("\n")
 
 # first, get the thing that contains them all, the span! store in variable names span_container
 # print span_conainer
+
 
 #second, get ll of the paragraphs in the span_container and store in a variable called paragraph_tags
 # print paragraph_tags
--- nytimes.py
+++ nytimes.py
@@ -1,14 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
- 
+
+#need to be careful with the site you choose
 base_url = 'http://www.nytimes.com'
 r = requests.get(base_url)
 soup = BeautifulSoup(r.text, "lxml")
 
 print("New York Times Headlines:")
- 
-for story_heading in soup.find_all(class_="story-heading"): 
-	if story_heading.a: 
-		print(story_heading.a.text.replace("\n", " ").strip())
+
+#LOOK AT THIS FOR PROJECT 2
+#use inspect element in browser to see "story-heading"
+#find all returns a list!!!
+for story_heading in soup.find_all(class_="story-heading"):
+	if story_heading.a: #if heading has an attribute where theres a link (links are a)
+		print(story_heading.a.text.replace("\n", " ").strip()) #print it out, \n is newline
 	else:
 		print(story_heading.contents[0].strip())
+
+
+##image example
+#img tag
+#attribute I want is the alt
+# .get('alt', 'No alternative text provided!') second value can be whatever I want
