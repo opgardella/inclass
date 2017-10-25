062f53c98be7c4795c60c87e8a41d974168ea9e9
Olivia Gardella <gardella@umich.edu>
2017-10-25 10:33:11 -0400

    worked on in class exercises

--- sqlite 10-23.py
+++ sqlite 10-23.py
@@ -1,5 +1,6 @@
 #sqlite exercise 10/23/17
 
+#make sure to import sqlite
 import sqlite3
 
 conn = sqlite3.connect('ages.sqlite')  #if ages doesnt exist, it automatically makes it for you
@@ -8,6 +9,7 @@ cur = conn.cursor()
 cur.execute('DROP TABLE IF EXISTS Ages')
 cur.execute('CREATE TABLE Ages (name VARCHAR, age INTEGER)')
 
+#inserting the names and ages
 cur.execute('INSERT INTO Ages (name, age) VALUES (?,?)', ('Kareem', 40))   #the ? is a placeholder, Kareem goes into first ?, 40 into 2nd ?
 cur.execute('INSERT INTO Ages (name, age) VALUES (?,?)', ('Oluwademilade', 36))
 cur.execute('INSERT INTO Ages (name, age) VALUES (?,?)', ('Aahana', 30))
@@ -15,7 +17,7 @@ cur.execute('INSERT INTO Ages (name, age) VALUES (?,?)', ('Ahtasham', 30))
 cur.execute('INSERT INTO Ages (name, age) VALUES (?,?)', ('Murry', 26))
 cur.execute('INSERT INTO Ages (name, age) VALUES (?,?)', ('Elise', 16))
 
-
+#selecting
 cur.execute('SELECT hex(name || age) AS X FROM Ages ORDER BY X')
 
 conn.commit()
